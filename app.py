c0eb5faaab705001a11cae8cc168ba7ce4ba05c6
Jerrick <jermil2990@gmail.com>
2025-05-12 00:23:28 -0500

    Update 3: Game Complete
    the game is finished with fully functional minigame

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-# write 'hello world' to the console
-print("hello world")
 
 # create rock, paper, scissors game
-
 # get game intro, get input from user to pick rock, paper, or scissors / lower input from user also check string input
-
 # inform if the user won, lost, or tied, display score and ask if player can play again, if user types screen play again
-
-print("Welcome to Rock, Paper, Scissors!")
 import random
 
+print("Welcome to Rock, Paper, Scissors!")
+print("Round 1")
 print("Type 'rock', 'paper', or 'scissors' to play!")
+
+round = 1
+
 def play_game():
+    global round
+    gameScore = 0
     user_choice = input("Enter your choice: ").lower()
     choices = ["rock", "paper", "scissors"]
     computer_choice = random.choice(choices)
@@ -29,18 +30,22 @@ def play_game():
          (user_choice == "paper" and computer_choice == "rock") or \
          (user_choice == "scissors" and computer_choice == "paper"):
         print("You win!")
+        gameScore += 1
     else:
         print("You lose!")
 
-    # ask if player can play again with the text input 'again' or 'screen'
+    print(f"Current Score: {gameScore}")
+
     play_again = input("Do you want to play again? (yes type again - no type screen): ").lower()
     if play_again == "yes" or play_again == "again":
-        play_game()
+        round += 1
+        print("Round", round)
+        play_game() 
     elif play_again == "no" or play_again == "screen":
         print("Thanks for playing!")
+        print(f"Your final score is: {gameScore}")
     else:
-        print("Invalid input. Please type 'yes or again' or 'no or screen'.")
-        
+        print("Invalid input. Game Over, Thanks for playing!")
+
 
 play_game()
-
